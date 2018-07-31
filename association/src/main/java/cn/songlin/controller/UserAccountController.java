e087aec80d8f659dc2aa049d09360c88043176b2
lsl <liuslc@yonyou.com>
2018-07-31 16:24:01 +0800

    logger的sql打印

--- association/src/main/java/cn/songlin/controller/UserAccountController.java
+++ association/src/main/java/cn/songlin/controller/UserAccountController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,7 +41,7 @@ public class UserAccountController {
 	@PostMapping("login")
 	@Monitor
 	@ApiOperation(value = "用户登录")
-	public ResponseEntity<String> login(UserLoginDto userLoginDto) {
+	public ResponseEntity<String> login(@RequestBody UserLoginDto userLoginDto) {
 		UserAccount userAccount = userAaccountService.login(userLoginDto);
 		if (userAccount != null) {// 登陆成功
 			// 写入缓存
--- association/src/main/java/cn/songlin/dto/UserLoginDto.java
+++ association/src/main/java/cn/songlin/dto/UserLoginDto.java
@@ -4,26 +4,25 @@ import cn.songlin.utils.StringUtils;
 
 public class UserLoginDto {
 
-	private String name;
-	
+	private String userName;
+
 	private String password;
 
-	public String getName() {
-		return name;
+	public String getUserName() {
+		return userName;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setUserName(String userName) {
+		this.userName = userName;
 	}
 
 	public String getPassword() {
-		//MD5加密
+		// MD5加密
 		return StringUtils.getMd5(password);
 	}
 
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	
+
 }
